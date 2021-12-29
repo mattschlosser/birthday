a28769364fe0fa458e3eb0648ba551d2ff2fb7e3
Matt Schlosser <mattschlosser@users.noreply.github.com>
2021-12-29 08:29:42 -0700

    enable loading

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
     <button class="btn" @click="askReset">Clear Everything</button>
     <button class="btn" @click="() => editing = !editing">Add Birthdays</button>
     <input  type="file" class="btn" @change="load($event)"/>
+    <button class="btn" @click="load" type="button">Load</button>
     <a  class="btn" :href="saveFile" target="_blank">Save</a>
     <transition name="fade">
     <div v-if="editing">
@@ -184,6 +185,7 @@ export default {
      * Loads a saved file into the database
      */
     load(event) {
+      console.log(event);
       const f = new FileReader();
       f.readAsText(event.target.files[0]);
       const vm = this;
